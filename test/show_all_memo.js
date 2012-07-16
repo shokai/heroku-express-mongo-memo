414ebc23b3beefce5c6263b0e9ce7869c7a82f2d
Sho Hashimoto <hashimoto@shokai.org>
2012-07-16 11:31:32 +0900

    show all memo test

---
+++ test/show_all_memo.js
@@ -0,0 +1,16 @@
+require(__dirname+'/../models/memo');
+
+var _ = require('underscore');
+var mongoose = require('mongoose');
+var Memo = mongoose.model('Memo');
+
+Memo.find({}, function(err, docs){
+  if(err){
+    console.error(err);
+  }
+  else{
+    _.each(docs, function(i){
+      console.log(i.body + '  - ' + i.created_at);
+    });
+  }
+});

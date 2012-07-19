3c97c091e6f25f26c869dd5ca5aa4af204782e5b
Sho Hashimoto <hashimoto@shokai.org>
2012-07-19 17:44:18 +0900

    exit if mongoose connect failed

--- models/index.js
+++ models/index.js
@@ -1,6 +1,11 @@
 
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/memo')
+mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/memo', function(err){
+  if(err){
+    console.error(err);
+    process.exit(1);
+  }
+});
 
 var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
